87025da72221f0fab50645fee66b6ec13efab6b5
MachariaP <walburphinehas78@gmail.com>
2024-07-12 10:19:12 +0300

    Using variable to store a string

---
+++ Learn/c02/js/string-variable.js
@@ -0,0 +1,9 @@
+var userName;
+var message;
+userName = 'MOlly';
+message = 'See our upcoming range';
+
+var elName = document.getElementById('name');
+elName.textContent = userName;
+var elNote = document.getElementById('note');
+elNote.textContent = message;
